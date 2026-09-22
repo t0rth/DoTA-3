56a6119de40c34ec3086dac3eb57d956db66ba92
t0rth <shulginvladimir090897@gmail.com>
2026-09-22 22:05:15 +0700

    Сохранение и загрузка в json

---
+++ DoTA 3/CEnemyTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DoTA_3
+{
+    public class CEnemyTemplate
+    {
+        [JsonInclude] public string Name { get; private set; }
+        [JsonInclude] public string IconName { get; private set; }
+        [JsonInclude] public int BaseLife { get; private set; }
+        [JsonInclude] public double LifeModifier { get; private set; }
+        [JsonInclude] public int BaseGold { get; private set; }
+        [JsonInclude] public double GoldModifier { get; private set; }
+        [JsonInclude] public double SpawnChance { get; private set; }
+
+        public CEnemyTemplate(string name, string iconName, int baseLife,
+            double lifeModifier, int baseGold,
+            double goldModifier, double spawnChance)
+        {
+            Name = name;
+            IconName = iconName;
+            BaseLife = baseLife;
+            LifeModifier = lifeModifier;
+            BaseGold = baseGold;
+            GoldModifier = goldModifier;
+            SpawnChance = spawnChance;
+        }
+    }
+}
---
+++ DoTA 3/CEnemyTemplateList.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using System.IO;
+
+namespace DoTA_3
+{
+    public class CEnemyTemplateList
+    {
+        //Список противников из класса CEnemyTemplate
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+        public void SaveToJson(string path)
+        {
+            string jsonString = JsonSerializer.Serialize(enemies);
+            File.WriteAllText(path, jsonString);
+        }
+
+        public void AddEnemy(string name, string iconName, int baseLife,
+                     double lifeModifier, int baseGold,
+                     double goldModifier, double spawnChance)
+        {
+            enemies.Add(new CEnemyTemplate(name, iconName, baseLife,
+                                           lifeModifier, baseGold,
+                                           goldModifier, spawnChance));
+        }
+        public List<string> GetListOfEnemyNames()
+        {
+            List<string> names = new List<string>();
+            foreach (CEnemyTemplate enemy in enemies)
+            {
+                names.Add(enemy.Name);
+            }
+            return names;
+        }
+
+        public void LoadFromJson(string path)
+        {
+            string jsonFromFile = File.ReadAllText(path);
+            JsonDocument doc = JsonDocument.Parse(jsonFromFile);
+
+            enemies.Clear();  // очищаем текущий список, чтобы не дублировать
+
+            foreach (JsonElement element in doc.RootElement.EnumerateArray())
+            {
+                string name = element.GetProperty("Name").GetString();
+                string iconName = element.GetProperty("IconName").GetString();
+                int baseLife = element.GetProperty("BaseLife").GetInt32();
+                double lifeModifier = element.GetProperty("LifeModifier").GetDouble();
+                int baseGold = element.GetProperty("BaseGold").GetInt32();
+                double goldModifier = element.GetProperty("GoldModifier").GetDouble();
+                double spawnChance = element.GetProperty("SpawnChance").GetDouble();
+
+                enemies.Add(new CEnemyTemplate(name, iconName, baseLife,
+                                               lifeModifier, baseGold,
+                                               goldModifier, spawnChance));
+            }
+        }
+    }
+}
